78d717964637346d80062bbd788e90ee4906b809
Rowt <venegramaryrose_bsit@plmun.edu.ph>
2024-11-29 14:34:30 +0800

    Add files via upload

--- header.php
+++ header.php
@@ -1,25 +1,97 @@
-<div class="header">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                    <a href="index.php"><img src="images/logo.png" alt=""></a>
-                </div>
-                <div class="col-lg-8 col-md-4 col-sm-12 col-xs-12">
-                    <div class="navigation">
-                        <div id="navigation">
-                            <ul>
-                                <li class="active"><a href="index.php" title="Home">Home</a></li>
-                                <li><a href="service-list.php" title="Service List">Service List</a>
-                                   
-                                </li>
-                               
-                                <li><a href="contact.php" title="Contact Us">Contact</a> </li>
-                                <li><a href="appointment.php" title="Styleguide">Book Appointment</a> </li>
-                                <li><a href="admin/index.php" title="Contact Us">Admin</a> </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="sticky-header header-section ">
+      <div class="header-left">
+        <!--toggle button start-->
+        <button id="showLeftPush"><i class="fa fa-bars"></i></button>
+        <!--toggle button end-->
+        <!--logo -->
+        <div class="logo">
+          <a href="index.html">
+            <h1>Salon Management</h1>
+            <span>AdminPanel</span>
+          </a>
         </div>
+        <!--//logo-->
+       
+       
+        <div class="clearfix"> </div>
+      </div>
+      <div class="header-right">
+        <div class="profile_details_left"><!--notifications of menu start -->
+          <ul class="nofitications-dropdown">
+            <?php
+$ret1=mysqli_query($con,"select ID,Name from  tblappointment where Status=''");
+$num=mysqli_num_rows($ret1);
+
+?>  
+            <li class="dropdown head-dpdn">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-bell"></i><span class="badge blue"><?php echo $num;?></span></a>
+              
+              <ul class="dropdown-menu">
+                <li>
+                  <div class="notification_header">
+                    <h3>You have <?php echo $num;?> new notification</h3>
+                  </div>
+                </li>
+                <li>
+            
+                   <div class="notification_desc">
+                     <?php if($num>0){
+while($result=mysqli_fetch_array($ret1))
+{
+            ?>
+                 <a class="dropdown-item" href="view-appointment.php?viewid=<?php echo $result['ID'];?>">New appointment received from <?php echo $result['Name'];?> </a><br />
+<?php }} else {?>
+    <a class="dropdown-item" href="all-appointment.php">No New Appointment Received</a>
+        <?php } ?>
+                           
+                  </div>
+                  <div class="clearfix"></div>  
+                 </a></li>
+                 
+                
+                 <li>
+                  <div class="notification_bottom">
+                    <a href="new-appointment.php">See all notifications</a>
+                  </div> 
+                </li>
+              </ul>
+            </li> 
+          
+          </ul>
+          <div class="clearfix"> </div>
+        </div>
+        <!--notification menu end -->
+        <div class="profile_details">  
+        <?php
+$adid=$_SESSION['bpmsaid'];
+$ret=mysqli_query($con,"select AdminName from tbladmin where ID='$adid'");
+$row=mysqli_fetch_array($ret);
+$name=$row['AdminName'];
+
+?> 
+          <ul>
+            <li class="dropdown profile_details_drop">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                <div class="profile_img"> 
+                  <span class="prfil-img"><img src="images/download (1).png" alt="" width="50" height="60"> </span> 
+                  <div class="user-name">
+                    <p><?php echo $name; ?></p>
+                    <span>Administrator</span>
+                  </div>
+                  <i class="fa fa-angle-down lnr"></i>
+                  <i class="fa fa-angle-up lnr"></i>
+                  <div class="clearfix"></div>  
+                </div>  
+              </a>
+              <ul class="dropdown-menu drp-mnu">
+                <li> <a href="change-password.php"><i class="fa fa-cog"></i> Settings</a> </li> 
+                <li> <a href="admin-profile.php"><i class="fa fa-user"></i> Profile</a> </li> 
+                <li> <a href="index.php"><i class="fa fa-sign-out"></i> Logout</a> </li>
+              </ul>
+            </li>
+          </ul>
+        </div>  
+        <div class="clearfix"> </div> 
+      </div>
+      <div class="clearfix"> </div> 
     </div>
